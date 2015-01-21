587019f694b16bbb3bfd84969c67c3e6ff2c96ee
Oscar Giraldo Castillo <oscar.gi.cast@gmail.com>
2015-01-21 11:55:20 -0500

    import dj_database_url to use 12factor db urls

--- landing/landing/settings.py
+++ landing/landing/settings.py
@@ -1,28 +1,17 @@
-"""
-Django settings for landing project.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.7/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.7/ref/settings/
-"""
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import dj_database_url
+
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', ' ')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# Default mode is set to False
+DEBUG = bool(os.environ.get('DEBUG', False))
 
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = DEBUG
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split()
 
@@ -52,16 +41,19 @@ ROOT_URLCONF = 'landing.urls'
 
 WSGI_APPLICATION = 'landing.wsgi.application'
 
-
 # Database
-# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if DEBUG:
+    DATABASES = {
+        'default': dj_database_url.config(
+            default='sqlite:///%s' % os.path.join(BASE_DIR, 'db.sqlite3'),
+        ),
     }
-}
+else:
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=os.environ.get('POSTGRES_URL'),
+        ),
+    }
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
