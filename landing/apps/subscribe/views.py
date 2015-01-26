e4a6a602ab3b0cb777c60fb583a393dce49c53ac
Oscar Giraldo Castillo <oscar.gi.cast@gmail.com>
2015-01-26 13:08:38 -0500

    update subscribe_view

--- landing/apps/subscribe/views.py
+++ landing/apps/subscribe/views.py
@@ -2,6 +2,7 @@ import requests
 import json
 from django.shortcuts import render
 from django.http import JsonResponse
+from .models import Subscriptor
 
 
 URL = "http://198.143.158.218:28080/ZonaDelSaber/validarCliente.action"
@@ -29,6 +30,19 @@ def subscribe_view(request):
         response = requests.get(URL, params=payload, headers=headers)
         data = response.json()
         data["code"] = int(data.get("code"))
+        if data.get("code") == 200:
+            try:
+                if Subscriptor.objects.get(client=client) or \
+                        Subscriptor.objects.get(account=account):
+                    data["message"] = "the client have subscribed already"
+                    data["code"] = 403
+            except:
+                if request.method == "POST":
+                    Subscriptor.objects.create(
+                        client=client,
+                        account=account,
+                        coupon_used="",
+                    )
     except Exception, e:
         data["message"] = "the request is syntactically incorrect"
         data["code"] = 400
