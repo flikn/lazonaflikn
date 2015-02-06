ff7610661f2e6765336fc5b050c4800390d28487
Oscar Giraldo Castillo <oscar.gi.cast@gmail.com>
2015-02-06 16:04:26 -0500

    change url routers

--- landing/apps/home/urls.py
+++ landing/apps/home/urls.py
@@ -4,8 +4,8 @@ from .views import HomeView, LoginView, SignupView, SubscribeView
 
 urlpatterns = patterns(
     '',
-    url(r'^$', HomeView.as_view(), name="home"),
-    url(r'^login/$', LoginView.as_view(), name="login"),
+    url(r'^$', SignupView.as_view(), name="signup"),
     url(r'^signup/$', SignupView.as_view(), name="signup"),
+    url(r'^login/$', LoginView.as_view(), name="login"),
     url(r'^subscribe/$', SubscribeView.as_view(), name="subscribe"),
 )
