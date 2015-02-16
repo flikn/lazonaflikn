729adbef094b363bcbb0e82ca8e7e3a20bc82402
Oscar Giraldo Castillo <oscar.gi.cast@gmail.com>
2015-02-16 14:05:10 -0500

    Create RequiredExtraDataForm to validate extra data reques from the pipeline. Modify UserSignupForm

--- landing/apps/customuser/forms.py
+++ landing/apps/customuser/forms.py
@@ -1,23 +1,22 @@
 from django import forms
-from django.contrib import admin
-from django.contrib.auth.models import Group
-from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.forms import ReadOnlyPasswordHashField
-
+from django.core import validators
 from .models import MyUser
 
 
 class UserSignupForm(forms.ModelForm):
 
     """A form for creating new users."""
+    username = forms.CharField(
+        required=False,
+    )
 
     class Meta:
         model = MyUser
         fields = [
-            'email',
-            'first_name',
-            'last_name',
-            'password',
+            "email",
+            "first_name",
+            "last_name",
+            "password",
         ]
 
     def clean_password(self):
@@ -27,11 +26,28 @@ class UserSignupForm(forms.ModelForm):
             raise forms.ValidationError("Password min length is 6")
         elif len(password) > 255:
             raise forms.ValidationError("Password max length is 255")
-
         return password
 
+    def name_is_not_empty(self, raw_name):
+        name = " ".join(raw_name.split())
+        if not name:
+            raise forms.ValidationError("Name cannot be left empty")
+        return name
+
+    def clean_first_name(self):
+        first_name = self.name_is_not_empty(
+            self.cleaned_data.get("first_name")
+        )
+        first_name = first_name.split()[0].title()
+        return first_name
+
+    def clean_last_name(self):
+        last_name = self.name_is_not_empty(self.cleaned_data.get("last_name"))
+        last_name = last_name.title()
+        return last_name
+
     def save(self, commit=True):
-        user = super(UserCreationForm, self).save(commit=False)
+        user = super(UserSignupForm, self).save(commit=False)
         user.set_password(self.cleaned_data["password"])
         user.email = self.cleaned_data["email"]
         user.first_name = self.cleaned_data["first_name"]
@@ -52,3 +68,62 @@ class UserLoginForm(forms.Form):
         max_length=100,
         min_length=6,
     )
+
+
+class RequiredExtraDataForm(forms.Form):
+
+    """A form for require extra data through the pipeline."""
+
+    email = forms.EmailField(
+        max_length=50,
+        required=False,
+    )
+    first_name = forms.CharField(
+        max_length=50,
+        validators=[
+            validators.RegexValidator(
+                r"^[a-zA-Z ]",
+                "Invalid name."
+            ),
+        ],
+        required=False,
+    )
+    last_name = forms.CharField(
+        max_length=255,
+        validators=[
+            validators.RegexValidator(
+                r"^[a-zA-Z ]",
+                "Invalid name."
+            ),
+        ],
+        required=False,
+    )
+    password = forms.CharField(
+        max_length=255,
+        min_length=6,
+        required=False,
+    )
+
+    def name_is_not_empty(self, raw_name):
+        name = " ".join(raw_name.split())
+        if not name:
+            raise forms.ValidationError("Name cannot be left empty")
+        return name
+
+    def clean_first_name(self):
+        first_name = self.name_is_not_empty(
+            self.cleaned_data.get("first_name")
+        )
+        first_name = first_name.split()[0].title()
+        return first_name
+
+    def clean_last_name(self):
+        last_name = self.name_is_not_empty(self.cleaned_data.get("last_name"))
+        last_name = last_name.title()
+        return last_name
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if MyUser.objects.filter(email=email).exists():
+            raise forms.ValidationError("Email already exists")
+        return email
